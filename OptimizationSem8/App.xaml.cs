8768356244f653f921385199c6bbbd2c3cdaa324
SacarLite <vladen.bokovoi@mail.ru>
2025-03-07 18:47:34 +0300

    Добавьте файлы проекта.

---
+++ OptimizationSem8/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace OptimizationSem8
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
